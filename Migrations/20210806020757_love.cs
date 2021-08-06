692a167ffd9e1387e4a050e63b1de67407ef4b3c
Lovepreet Singh <LS052@atcltd.atc.org.nz>
2021-08-06 14:09:39 +1200

    update

---
+++ Migrations/20210806020757_love.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Student_Notice_Board_Final.Migrations
+{
+    public partial class love : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
